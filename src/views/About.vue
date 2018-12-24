fbd156bff64f79816fd9f9180f37be6878a8eda6
html5s <184636408@qq.com>
2018-12-24 17:29:33 +0800

    path 路径信息解析完毕

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <svg>
+    <path></path>
+  </svg>
 </template>
+<script>
+import svgPath from '../../public/json/svgPath'
+export default {
+  data(){
+    return{
+      sexType:'male',
+      maleFrontOne: [],
+      maleFrontTwo: [],
+      maleBackOne: [],
+      maleBackTwo: []
+    }
+  },
+  mounted(){
+    this.
+    fetch('svgPath').then(response=>response.json()).then(res=>{
+      console.log(res)
+    })
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
