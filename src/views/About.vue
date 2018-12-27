513355871d5ed8ae1f3a549d6ff0dec9be0d74e3
html5s <184636408@qq.com>
2018-12-27 10:53:56 +0800

    更新三级信息

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,28 +2,38 @@
   <div class="person-svg">
     <van-button type="primary" @click="turn()">正反转换</van-button>
     <!-- 人体svg热区图  -->
-    <svg id="full-body" x="0px" y="0px" viewBox="0 0 310 385" style="width:100%" v-bind:preserveAspectRatio="preserveAspectRatio?'xMidYMid slice':''"
-      xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <svg
+      id="full-body"
+      x="0px"
+      y="0px"
+      viewBox="0 0 310 385"
+      style="width:100%"
+      v-bind:preserveAspectRatio="preserveAspectRatio?'xMidYMid slice':''"
+      xml:space="preserve"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
       <image v-bind:xlink:href="imgPath" height="100%" width="100%" x="0" y="0"></image>
-      <path v-for="(value,index) in pathList" :key="index"
+      <path
+        v-for="(value,index) in pathList"
+        :key="index"
         class="layers layer-show"
-        fill="#27BFCF" 
-        fill-rule="evenodd" 
-        opacity=".3" 
-        v-bind:d="value.d" 
+        fill="#27BFCF"
+        fill-rule="evenodd"
+        opacity=".3"
+        v-bind:d="value.d"
         v-bind:transform="value.transform"
-        @click="getLevelTwo('HEAD_1',1)">
-      </path>
+        @click="getLevelTwo('HEAD_1',1)"
+      ></path>
     </svg>
 
     <!-- 人体部位层级数据 -->
     <van-popup v-model="show" position="bottom" :overlay="false" :lock-scroll="false">
-      
       <div class="popup-cell">
         <div class="popup-cell-title">请选择您的症状</div>
         <van-icon name="cross" color="#fff" @click="hideMenu"></van-icon>
       </div>
-      
+
       <!-- 二级菜单 -->
       <div v-if="!showNext" v-for="(value,index) in levelTwo" :key="index">
         <div class="cell-link van-hairline--bottom" @click="getLevelThree(value.code)">
@@ -33,28 +43,40 @@
       </div>
 
       <!-- 返回二级菜单 -->
-      <div v-if="showNext" class="level-one" @click="backLevel"><van-icon name="arrow-left" color="#1e84d8" size="16px"></van-icon>返回</div>
-      <!-- 三级菜单 -->
-      <div v-if="showNext" v-for="(item,indexs) in levelThree" :key="indexs+'-3'">
-        <div class="cell-link van-hairline--bottom">
-          <div class="popup-cell-title">{{item.name}}</div>
-          <van-icon name="arrow" color="#333" size="14px"></van-icon>
-        </div>
+      <div v-if="showNext" class="level-one" @click="backLevel">
+        <van-icon name="arrow-left" color="#1e84d8" size="16px"></van-icon>返回
       </div>
-
+      <!-- 三级菜单 -->
+      <van-checkbox-group v-model="checkResult" v-if="showNext">
+        <van-cell-group>
+          <van-cell
+            v-for="(item,indexs) in levelThree"
+            clickable
+            :key="indexs"
+            :title="item.name"
+            @click="toggle(index)"
+          >
+            <van-checkbox :name="item" ref="checkboxes"/>
+          </van-cell>
+        </van-cell-group>
+      </van-checkbox-group>
+      <van-button size="large" class="" v-if="showNext">下一步</van-button>
     </van-popup>
-
   </div>
 </template>
 <script>
 import svgPath from "../../public/json/svgPath";
-import { Popup, Toast, Button, Icon } from "vant";
+import { Popup, Toast, Button, Icon, Checkbox, CheckboxGroup, CellGroup, Cell } from "vant";
 export default {
   components: {
+    [Cell.name]: Cell,
     [Icon.name]: Icon,
     [Toast.name]: Toast,
     [Popup.name]: Popup,
-    [Button.name]: Button
+    [Button.name]: Button,
+    [Checkbox.name]: Checkbox,
+    [CellGroup.name]: CellGroup,
+    [CheckboxGroup.name]: CheckboxGroup
   },
 
   data() {
@@ -62,118 +84,127 @@ export default {
       show: false,
       showNext: false,
       sexType: "male",
-      faceType:'front',
+      faceType: "front",
       preserveAspectRatio: false,
       pathList: [],
-      levelTwo:[],
-      levelThree:[],
-      imgPath: require('../assets/male-f.png')
+      levelTwo: [],
+      levelThree: [],
+      checkResult: [],
+      imgPath: require("../assets/male-f.png"),
     };
   },
 
   mounted() {
-    this.sexType = this.$route.query.sexType
-    this.faceType = 'front'
-    if(this.$route.query.sexType == 'male'){
-      this.imgPath = require('../assets/male-f.png')
+    this.sexType = this.$route.query.sexType;
+    this.faceType = "front";
+    if (this.$route.query.sexType == "male") {
+      this.imgPath = require("../assets/male-f.png");
       this.pathList = svgPath.male.front.levelOne;
-    }else{
-      this.imgPath = require('../assets/female-f.png')
+    } else {
+      this.imgPath = require("../assets/female-f.png");
       this.pathList = svgPath.female.front.levelOne;
     }
   },
 
   methods: {
     hideMenu() {
-      this.show = false
-      this.showNext = false
-      this.preserveAspectRatio = false
+      this.show = false;
+      this.showNext = false;
+      this.preserveAspectRatio = false;
     },
-    getLevelTwo(paramName,level) {
-      if(level == 1){
-        let paths = svgPath[this.sexType][this.faceType]
-        this.pathList = paths.levelTwo
-        console.log(paths.levelTwo)
-        // this.preserveAspectRatio = true
-      }else{
-        fetch("http://cm.changrentech.com:8092/bodyparts.do?parentCode="+paramName)
+    getLevelTwo(paramName, level) {
+      fetch(
+        "http://cm.changrentech.com:8092/bodyparts.do?parentCode=" + paramName
+      )
         .then(response => response.json())
         .then(res => {
-          this.show = true
-          this.levelTwo = res.result
+          this.show = true;
+          this.levelTwo = res.result;
         })
         .catch(err => {
-          this.$toast({message:'检测异常，稍后再试',mask:false,duration:1500})
-          console.log(err);
-        });
-      }
-    },
-    getLevelThree(paramName){
-      fetch("http://cm.changrentech.com:8092/symptoms/symptomsList?code="+paramName+"&iscommon=false")
-        .then(response => response.json())
-        .then(res => {
-          console.log(res.result)
-          this.levelThree = res.result
-        })
-        .catch(err => {
-          this.$toast({message:'检测异常，稍后再试',mask:false,duration:1500})
+          this.$toast({
+            message: "检测异常，稍后再试",
+            mask: false,
+            duration: 1500
+          });
           console.log(err);
         });
     },
-    backLevel(){
-      this.showNext = false
+    getLevelThree(paramName) {
+      fetch(
+        "http://cm.changrentech.com:8092/symptoms/symptomsList?code=" +
+          paramName +
+          "&iscommon=false"
+      )
+        .then(response => response.json())
+        .then(res => {
+          console.log(res.result);
+          this.levelThree = res.result;
+          this.showNext = true
+        })
+        .catch(err => {
+          this.$toast({
+            message: "检测异常，稍后再试",
+            mask: false,
+            duration: 1500
+          });
+          console.log(err);
+        });
     },
-    turn(){
-      this.show = false
-      let sexType = this.sexType
-      let faceType = this.faceType
-      if(faceType == 'front'){
-        this.faceType = 'back'
-        this.imgPath = require('../assets/'+sexType+'-b.png')
-        let pathf = svgPath[sexType][this.faceType]
-        this.pathList = pathf.levelOne
-      }else{
-        this.faceType = 'front'
-        this.imgPath = require('../assets/'+sexType+'-f.png')
-        let pathb = svgPath[sexType][this.faceType]
-        this.pathList = pathb.levelOne
+    backLevel() {
+      this.showNext = false;
+    },
+    turn() {
+      this.show = false;
+      let sexType = this.sexType;
+      let faceType = this.faceType;
+      if (faceType == "front") {
+        this.faceType = "back";
+        this.imgPath = require("../assets/" + sexType + "-b.png");
+        let pathf = svgPath[sexType][this.faceType];
+        this.pathList = pathf.levelOne;
+      } else {
+        this.faceType = "front";
+        this.imgPath = require("../assets/" + sexType + "-f.png");
+        let pathb = svgPath[sexType][this.faceType];
+        this.pathList = pathb.levelOne;
       }
     }
   }
 };
 </script>
 <style lang="scss">
-.person-svg{
-  width:100vw;
-  height:100vh;
+.person-svg {
+  width: 100vw;
+  height: 100vh;
 }
 #full-body {
   display: block;
   height: 96%;
-  transition: width .5s;
-  -webkit-transition: width .5s;
-  -moz-transition: width .5s;
+  transition: width 0.5s;
+  -webkit-transition: width 0.5s;
+  -moz-transition: width 0.5s;
   .layers {
     cursor: pointer;
     display: none;
     opacity: 0;
     outline: 0;
-    transition: opacity .5s linear;
+    transition: opacity 0.5s linear;
     &:hover {
-      opacity: .525;
+      opacity: 0.525;
       outline: 0;
-      transition: opacity .5s linear;
+      transition: opacity 0.5s linear;
     }
   }
-  .layer-show{
-    display:block;
+  .layer-show {
+    display: block;
   }
 }
 
 .van-popup {
   height: 30%;
 }
-.level-one{
+.level-one {
   font-size: 16px;
   color: #1e84d8;
   line-height: 45px;
@@ -206,5 +237,13 @@ export default {
     font-size: 14px;
     color: #333;
   }
+}
+.van-cell__title{
+  flex-basis: 90%;
+  display: flex;
+  justify-content: space-between;
+}
+.van-cell__value{
+  flex-basis: 10%;
 }
 </style>
